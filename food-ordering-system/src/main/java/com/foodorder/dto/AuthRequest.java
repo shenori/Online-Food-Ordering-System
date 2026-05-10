22e1eed266e1fff1e681f413443960108ac58540
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-05-10 18:32:37 +0530

    Verify email and assword with AuthRequest.java

---
+++ food-ordering-system/src/main/java/com/foodorder/dto/AuthRequest.java
@@ -0,0 +1,18 @@
+package com.foodorder.food_ordering_system.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
