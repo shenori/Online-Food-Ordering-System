37dbefa9b477a331f60875d133c142029b701dd4
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-05-14 10:45:33 +0530

    Set-up the User.java file

---
+++ food-ordering-system/src/main/java/com/foodorder/food_ordering_system/entity/User.java
@@ -0,0 +1,33 @@
+package com.foodorder.food_ordering_system.entity;
+
+import com.foodorder.food_ordering_system.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Cart cart;
+}
