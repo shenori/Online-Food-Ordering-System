9891d648ad2b7df6dfaf004af525258e80bc79dc
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-05-14 10:51:01 +0530

    Re-adding some files to entity folder

---
+++ food-ordering-system/src/main/java/com/foodorder/food_ordering_system/entity/Order.java
@@ -0,0 +1,37 @@
+package com.foodorder.food_ordering_system.entity;
+
+import com.foodorder.food_ordering_system.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    private LocalDateTime orderDate;
+    private Double totalAmount;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+}
---
+++ food-ordering-system/src/main/java/com/foodorder/food_ordering_system/entity/Payment.java
@@ -0,0 +1,31 @@
+package com.foodorder.food_ordering_system.entity;
+
+import com.foodorder.food_ordering_system.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    private LocalDateTime paymentDate;
+
+    @OneToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
